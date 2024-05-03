043a224accc7f7464a219d5634960a1582c3e79e
Lia martinez <166756277+Liasonm@users.noreply.github.com>
2024-05-03 19:34:44 +0000

    proyecto acabado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = excuseGenerator();
 };
+
+/** 1-Index.html
+ * 2-crear variables
+ * 3-Funcion random
+ * 4-Modificar HTML
+ */
+
+function excuseGenerator() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  const whoRandom = Math.floor(Math.random() * who.length);
+  const actionRandom = Math.floor(Math.random() * action.length);
+  const whatRandom = Math.floor(Math.random() * what.length);
+  const whenRandom = Math.floor(Math.random() * when.length);
+
+  const excuse =
+    who[whoRandom] + action[actionRandom] + what[whatRandom] + when[whenRandom];
+  return excuse;
+}
